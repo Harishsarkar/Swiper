27c8f3d7caa61367a533a2d2a1ed109290d1a0a7
Harishsarkar <46566124+Harishsarkar@users.noreply.github.com>
2022-05-17 13:24:59 +0530

    Create content.php

---
+++ content.php
@@ -0,0 +1,259 @@
+<div class="entry-content herald-entry-content">
+
+	<?php if( herald_is_paginated_post() && strpos( herald_get_option( 'single_paginated_nav_position'), 'above' ) !== false ) : ?>
+		<?php get_template_part( 'template-parts/single/paginated-nav' ); ?>
+	<?php endif; ?>
+
+	<?php if( herald_get_post_display( 'headline' ) && has_excerpt() ): ?>
+		<div class="entry-headline h5"><?php echo get_the_excerpt(); ?></div>
+	<?php endif; ?>
+
+	<?php if( herald_get_post_display( 'ad_above' ) ) : ?>
+		<?php get_template_part('template-parts/ads/above-single'); ?>
+	<?php endif; ?>
+
+	<?php $share_option = herald_get_option('single_share_content_position'); ?>
+	<?php if (  strpos($share_option, 'above') !== false ): ?>
+		<?php get_template_part( 'template-parts/single/share' ); ?>
+	<?php endif ?>
+	
+	<?php the_content(); ?>
+
+	<?php if( herald_is_paginated_post() && strpos( herald_get_option( 'single_paginated_nav_position'), 'below' ) !== false ) : ?>
+		<?php get_template_part( 'template-parts/single/paginated-nav' ); ?>
+	<?php endif; ?>
+
+<hr>
+<p class="has-text-align-center"> Give Credits Wherever You Share/Use It. </p>
+<hr>
+<center><a class="mks_button mks_button_large rounded" href="https://youtu.be/zgy6e87koCY" target="_self" style="color: #FFFFFF;background-color: #a70000;" rel="noopener nofollow" title="How to Watch/Download?"><i class="fa fa-youtube-play fa-lg"></i>How to Watch/Download?</a></center>
+<hr>
+<center><a class="mks_button mks_button_large rounded" href="https://t.me/+F4Cyej9LeW43NGQ1" target="_self" style="color: #FFFFFF;background-color: #000;" rel="noopener nofollow" title="Join TMI (Telegram)"><i class="fa fa-telegram fa-lg"></i>Join TMI (Telegram)</a> </center>
+<hr>
+<div class="wp-block-image"><figure class="aligncenter size-full is-resized"><a href="https://toonmixindia.me/dead-link-report/"><img src="https://i.postimg.cc/HLnRFY7j/Links-Not-Working-Report-Dead-Link-Here-300x83-2.png" alt="" class="wp-image-9754" width="275" height="76"/></a></figure></div>
+	
+	
+<div class="gs_logo_area gs_logo_area_1 slider1" style="opacity: 0; visibility: hidden;">
+<div class="gs_logo_area--inner">
+<div class="gs_logo_container gs_carousel_swiper gs_logo_fix_height_and_center" data-carousel-config="{&quot;direction&quot;:&quot;horizontal&quot;,&quot;speed&quot;:2600,&quot;isAutoplay&quot;:true,&quot;autoplayDelay&quot;:2000,&quot;loop&quot;:true,&quot;pauseOnHover&quot;:true,&quot;ticker&quot;:false,&quot;navs&quot;:false,&quot;navs_pos&quot;:&quot;left-right-out&quot;,&quot;dots&quot;:false,&quot;dynamic_dots&quot;:true,&quot;slideSpace&quot;:10,&quot;slidesPerGroup&quot;:1,&quot;desktopLogos&quot;:5,&quot;tabletLogos&quot;:4,&quot;mobileLogos&quot;:3,&quot;reverseDirection&quot;:false}">
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+
+<a href='https://toonmixindia.me/?s=avenger'><img src='https://i.postimg.cc/Dz4Jw6Wt/Ironman4-182x300.png'/></a>
+
+</div>
+</div>
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+
+<a href='https://toonmixindia.me/?s=slugterra'>
+<img src='https://i.postimg.cc/BZTFFmh3/Slugterra3-182x300.png'/></a>
+
+</div>
+</div>
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+
+<a href='https://toonmixindia.me/?s=naruto'>
+<img src='https://i.postimg.cc/d0gC2pPs/Pics-Art-08-23-12-01-31.png'/></a>
+
+</div>
+</div>
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+
+<a href='https://toonmixindia.me/?s=doraemon'>
+<img src='https://i.postimg.cc/P5r1T1Qj/Doratest4-bak-182x300.png'/></a>
+
+</div>
+</div>
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+
+<a href='https://toonmixindia.me/?s=beyblade'><img src='https://i.postimg.cc/vBDPBp7G/Beyblade4-182x300.png'/></a>
+
+</div>
+</div>
+
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+
+<a href='https://toonmixindia.me/?s=shinchan'><img src='https://i.postimg.cc/vTNXdcFM/Shinchan4-182x300.png'/></a>
+
+</div>
+</div>
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+
+<a href='https://toonmixindia.me/?s=Dragon+Ball'>
+<img src='https://i.postimg.cc/FHf81XDD/Dragonball5-182x300.png'/></a>
+
+</div>
+</div>
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+
+<a href='https://toonmixindia.me/?s=power+rangers'><img src='https://i.postimg.cc/HWQN7t01/1631788820654.png'/></a>
+
+</div>
+</div>
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+
+<a href='https://toonmixindia.me/?s=pokemon'><img src='https://i.postimg.cc/rwggNns7/Pokemon.png'/></a>
+
+</div>
+</div>
+<div class="swiper-wrapper">
+<div class="swiper-slide" >
+
+<a href='https://toonmixindia.me/?s=Ben+10'>
+<img src='https://i.postimg.cc/CMPmTfJq/Ben-10.png'/></a>
+
+</div>
+</div>
+	<div class="swiper-wrapper">
+<div class="swiper-slide" >
+
+<a href='https://toonmixindia.me/?s=Spiderman'>
+<img src='https://i.postimg.cc/yYr839zB/Spiderman4-182x300.png'/></a>
+
+</div>
+</div>
+	<div class="swiper-wrapper">
+<div class="swiper-slide" >
+
+<a href='https://toonmixindia.me/?s=Miraculous'>
+<img src='https://i.postimg.cc/VsGDGjwf/Miraclous.png'/></a>
+
+</div>
+</div>
+</div> </div></div>
+	<!-- Link Swiper's CSS -->
+<link href="https://cdn.jsdelivr.net/gh/Harishsarkar/newswiper@main/gs-logo.min.css" id="gs-logo-public-css" media="all" rel="stylesheet" type="text/css"/>
+<script id="gs-images-loaded-js" src="https://cdn.jsdelivr.net/gh/Harishsarkar/newswiper@main/images-loaded.min.js" type="text/javascript"></script>
+<script id="gs-swiper-js" src="https://cdn.jsdelivr.net/gh/Harishsarkar/newswiper@main/swiper.min.js" type="text/javascript"></script>
+<script id="gs-logo-public-js" src="https://cdn.jsdelivr.net/gh/Harishsarkar/newswiper@main/gs-logo.min.js" type="text/javascript"></script>
+	<link href='https://cdn.jsdelivr.net/gh/Harishsarkar/Swiper@main/swiper-bundle.min.css' rel='stylesheet'/>
+<!-- Link Swiper's CSS -->
+
+<style>
+	.swiper-container {
+		  height:auto;
+		  width:auto;
+       
+  }
+      .swiper-slide {
+    background-position: center;
+    background-size: cover;
+    background: transparent;
+    width: 145px;
+	margin-right: -30px;
+}
+ 
+      .swiper-slide img {
+   display: inline-block!important;
+    height: auto;
+    margin: 0 auto;
+    max-height: 100%;
+    max-width: 100%;
+    -webkit-transition: -webkit-filter .25s ease-in-out;
+    transition: -webkit-filter .25s ease-in-out;
+    -o-transition: filter .25s ease-in-out;
+    transition: filter .25s ease-in-out;
+    transition: filter .25s ease-in-out,-webkit-filter .25s ease-in-out;
+    width: 100%;
+}
+	  .swiper-slide img:hover{
+    webkit-filter: grayscale(100%);
+    filter: grayscale(100%);
+    -webkit-filter: grayscale(1);
+    filter: grayscale(1);
+      }
+
+	@media (max-width: 767px) and (min-width: 567px) {
+.swiper-slide {
+	margin-right: 3px;
+	}
+}	
+@media (max-width: 837px) and (min-width: 768px) {
+.swiper-slide {
+	margin-right: 5px;
+	}
+}
+	@media (max-width: 1023px) and (min-width: 905px) {
+.swiper-slide {
+	margin-right: -50px;
+	}
+}
+	@media (max-width: 1359px) and (min-width: 1249px) {
+.swiper-slide {
+	margin-right: 0px;
+	}
+}
+   @media (max-width: 566px) and (min-width: 450px) {
+.swiper-slide {
+	margin-right: 20px;
+	width: 126px
+	}
+}
+	   @media (max-width: 449px) and (min-width: 426px) {
+.swiper-slide {
+	margin-right: 40px;
+	width: 125px
+	}
+}
+	@media (max-width: 427px) and (min-width: 356px) {
+.swiper-slide {
+	margin-right: 23px;
+		width: 110px;
+	}
+}
+	@media (max-width: 367px) and (min-width: 302px) {
+.swiper-slide {
+	margin-right: 30px;
+		width: 98px;
+	}
+}
+	@media (max-width: 303px) and (min-width: 263px) {
+.swiper-slide {
+	margin-right: 40px;
+		width: 98px;
+	}
+}
+		@media (max-width: 264px) and (min-width: 214px) {
+.swiper-slide {
+	margin-right: 45px;
+		width: 88px;
+	}
+}
+		@media (max-width: 215px) and (min-width: 195px) {
+.swiper-slide {
+	margin-right: 50px;
+		width: 80px;
+	}
+}
+		@media (max-width: 196px) and (min-width: 0px) {
+.swiper-slide {
+	margin-right: 60px;
+		width: 70px;
+	}
+}
+    </style>
+
+<br>
+	
+	<?php if( herald_get_post_display( 'tags' ) && has_tag() ) : ?>
+		<div class="meta-tags">
+			<span><?php echo __herald('tagged_as'); ?></span><?php the_tags( false, ' ', false ); ?>
+		</div>
+	<?php endif; ?>
+
+	<?php if (  strpos($share_option, 'below') !== false ): ?>
+		<?php get_template_part( 'template-parts/single/share' ); ?>
+	<?php endif ?>
+
+	<?php if( herald_get_post_display( 'ad_below' ) ) : ?>
+		<?php get_template_part('template-parts/ads/below-single'); ?>
+	<?php endif; ?>
+</div>
